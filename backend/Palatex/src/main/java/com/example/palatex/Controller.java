c7c15a982a4e9b8bae860136eed09a880f71afbf
Artermis <nopphadon.phanwong@gmail.com>
2021-11-24 15:55:43 +0700

    Update Controller

--- backend/Palatex/src/main/java/com/example/palatex/Controller.java
+++ backend/Palatex/src/main/java/com/example/palatex/Controller.java
@@ -2,11 +2,16 @@ package com.example.palatex;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class Controller {
     @RequestMapping("/test")
     public String test(){
         return "Hi! Test";
+    }
+    @RequestMapping("/getPrice")
+    public String getPrice(){
+        return "Price is ...";
     }
 }
