ed982e0299aed93c5d35dda2aa858feda51547ca
Artermis <nopphadon.phanwong@gmail.com>
2021-11-27 21:44:28 +0700

    Update

--- backend/Palatex/src/main/java/com/example/palatex/Controller.java
+++ backend/Palatex/src/main/java/com/example/palatex/Controller.java
@@ -12,15 +12,15 @@ public class Controller {
     }
     @RequestMapping("/getPrice")
     public String getPrice(){
-//        WebClient.ResponseSpec out = WebClient.create("https://dataapi.moc.go.th")
-//                .get()
-//                .uri(uriBuilder -> uriBuilder
-//                        .path("/gis-product-prices")
-//                        .queryParam("product_id", "W16025")
-//                        .queryParam("from_date", "2018-11-01")
-//                        .queryParam("to_date", "2018-11-24")
-//                        .build())
-//                .retrieve();
+        WebClient.ResponseSpec out = WebClient.create("https://dataapi.moc.go.th")
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/gis-product-prices")
+                        .queryParam("product_id", "W16025")
+                        .queryParam("from_date", "2018-11-01")
+                        .queryParam("to_date", "2018-11-24")
+                        .build())
+                .retrieve();
         WebClient webClient = WebClient.create();
         String responseJson = webClient.get()
                 .uri("https://dataapi.moc.go.th/gis-product-prices?product_id=W16025&from_date=2018-11-01&to_date=2018-11-24")
