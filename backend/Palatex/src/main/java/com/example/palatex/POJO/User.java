35c7a277a94f93d12f5e843e79a0eb783f90f0d7
kaolnwza <kaow_jijuy999@hotmail.com>
2021-12-10 03:48:22 +0700

    fix code syntax

--- backend/Palatex/src/main/java/com/example/palatex/POJO/User.java
+++ backend/Palatex/src/main/java/com/example/palatex/POJO/User.java
@@ -1,26 +1,32 @@
 package com.example.palatex.POJO;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 // This user schema for map value from database
+@Getter
+@Setter
+
 @Data
 @Document("user")
-public class user {
+public class User {
     @Id
     private String _id;
+    private int memberId;
     private String firstname;
     private String lastname;
-    private int memberID;
 
-    public user (){}
 
-    public user(String _id, String firstName, String lastName, int memberID){
+    public User(){}
+
+    public User(String _id, int memberId, String firstname, String lastname){
         this._id = _id;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.memberID = memberID;
+        this.memberId = memberId;
     }
 
 
