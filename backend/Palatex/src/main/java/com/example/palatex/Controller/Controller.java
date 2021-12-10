ea5107007a02ca287bc53f2aae5c0e6253cd05fa
Artermis <nopphadon.phanwong@gmail.com>
2021-12-11 00:28:16 +0700

    :sparkles:  Add RabbitMQ and test it!

--- backend/Palatex/src/main/java/com/example/palatex/Controller/Controller.java
+++ backend/Palatex/src/main/java/com/example/palatex/Controller/Controller.java
@@ -3,6 +3,8 @@ package com.example.palatex.Controller;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,10 @@ import javax.net.ssl.SSLException;
 
 @RestController
 public class Controller {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     //This router for test something
     @RequestMapping("/test")
     public String test(){
@@ -25,6 +31,11 @@ public class Controller {
     @RequestMapping("/getTest")
     public String getPrice(){
         return "test";
+    }
+
+    @RequestMapping("/message")
+    public void testMessage(){
+        rabbitTemplate.convertAndSend("sop", "tester", "Golf");
     }
 }
 
