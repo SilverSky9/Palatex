dc8f8fcaf88bd5768939254d562e796a766a114f
Artermis <nopphadon.phanwong@gmail.com>
2021-12-11 22:01:56 +0700

    Add POJO file

---
+++ backend/Palatex/src/main/java/com/example/palatex/POJO/Latex.java
@@ -0,0 +1,35 @@
+package com.example.palatex.POJO;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Getter
+@Setter
+
+@Data
+@Document("latex")
+public class Latex {
+    @Id
+    private String _id;
+    private int latex_id;
+    private float price;
+    private Date date; //not sure
+
+    public Latex(){}
+
+    public Latex(String _id, int latex_id, float price,Date date){
+        this._id = _id;
+        this.latex_id = latex_id;
+        this.price = price;
+        this.date = date;
+
+
+    }
+
+}
---
+++ backend/Palatex/src/main/java/com/example/palatex/POJO/LatexSheet.java
@@ -0,0 +1,36 @@
+package com.example.palatex.POJO;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Getter
+@Setter
+
+@Data
+@Document("latexsheet")
+public class LatexSheet {
+
+    @Id
+    private String _id;
+    private int latex_sheet_id;
+    private Date date;
+    private float unit;
+
+    public LatexSheet(){}
+
+    public LatexSheet(String _id, int latex_sheet_id, Date date,float unit){
+        this._id = _id;
+        this.latex_sheet_id = latex_sheet_id;
+        this.date = date;
+        this.unit = unit;
+
+
+    }
+
+
+}
