ce29097a586871d25ac174a4556dabbbad54547d
Artermis <nopphadon.phanwong@gmail.com>
2021-12-12 00:11:43 +0700

    :+1: Update Transaction controller

--- backend/Palatex/src/main/java/com/example/palatex/Controller/transactionController.java
+++ backend/Palatex/src/main/java/com/example/palatex/Controller/transactionController.java
@@ -1,13 +1,25 @@
 package com.example.palatex.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.palatex.Service.transactionService;
+import com.example.palatex.POJO.Transaction;
+
+import java.util.List;
 
 @RestController
 public class transactionController {
     @Autowired
     private transactionService transactionService;
+
+    @RequestMapping(value = "/getT", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllTransaction(){
+        List<Transaction> transactions = transactionService.getAllTransactionService();
+        return  ResponseEntity.ok(transactions);
+    }
 
 
 
