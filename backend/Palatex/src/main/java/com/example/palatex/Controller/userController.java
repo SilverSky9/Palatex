de29b218da6e3d8ed8d83aeabe8a805936a29bce
Artermis <nopphadon.phanwong@gmail.com>
2021-12-12 13:41:40 +0700

    Update add user by RequestBody JSON

--- backend/Palatex/src/main/java/com/example/palatex/Controller/userController.java
+++ backend/Palatex/src/main/java/com/example/palatex/Controller/userController.java
@@ -5,16 +5,14 @@ import com.example.palatex.Service.userService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
 //User router for query data
 @RestController
+@RequestMapping(value = "/user")
 public class userController {
     @Autowired
     private userService userService;
@@ -22,22 +20,27 @@ public class userController {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @RequestMapping(value = "/getalluser", method = RequestMethod.GET)
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public ResponseEntity<?> getAllUser(){
         List<User> users = userService.getAllUsersService();
         return ResponseEntity.ok(users);
     }
 
-    @RequestMapping(value = "/adduser/{memberId}/{firstname}/{lastname}", method = RequestMethod.POST)
-    public ResponseEntity<?> addUser(@PathVariable("memberId") int memberId,
-                                     @PathVariable("firstname") String firstname,
-                                     @PathVariable("lastname") String lastname){
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ResponseEntity<?> addUser(@RequestBody  User user){
+        userService.addUserService(user);
+        return ResponseEntity.ok("Save user!");
 
-        User u = userService.addUserService(new User(null, memberId, firstname, lastname));
-        return ResponseEntity.ok(u);
     }
+//    public ResponseEntity<?> addUser(@PathVariable("memberId") int memberId,
+//                                     @PathVariable("firstname") String firstname,
+//                                     @PathVariable("lastname") String lastname){
+//
+//        User u = userService.addUserService(new User(null, memberId, firstname, lastname));
+//        return ResponseEntity.ok(u);
+//    }
 
-    @RequestMapping(value = "/findbymemberid/{memberId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/id/{memberId}", method = RequestMethod.GET)
     public ResponseEntity<?> addUser(@PathVariable("memberId") int memberId){
 
         User u = userService.getUserByMemberIdService(memberId);
